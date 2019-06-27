261a2ee3c34ae8eff9d492857eef368c882cebcc
Dustin Woo <hi@dustinwoo.com>
2019-06-26 17:55:30 -0700

    Document RideType

--- networking/src/main/java/com/lyft/networking/apiObjects/RideType.java
+++ networking/src/main/java/com/lyft/networking/apiObjects/RideType.java
@@ -2,16 +2,20 @@ package com.lyft.networking.apiObjects;
 
 import com.google.gson.annotations.SerializedName;
 import com.lyft.networking.apiObjects.internal.Validatable;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class RideType implements Validatable {
 
+    @NotNull
     @SerializedName("ride_type")
     public final String ride_type;
 
+    @NotNull
     @SerializedName("display_name")
     public final String display_name;
 
+    @NotNull
     @SerializedName("seats")
     public final Integer seats;
 
@@ -19,19 +23,26 @@ public class RideType implements Validatable {
     @SerializedName("image_url")
     public final String image_url;
 
+    @NotNull
     @SerializedName("pricing_details")
     public final PricingDetails pricing_details;
+
+    @Nullable
+    @SerializedName("scheduled_pricing_details")
+    public final PricingDetails scheduled_pricing_details;
 
     public RideType(String ride_type,
                     String display_name,
                     Integer seats,
                     String image_url,
-                    PricingDetails pricing_details) {
+                    PricingDetails pricing_details,
+                    PricingDetails scheduled_pricing_details) {
         this.ride_type = ride_type;
         this.display_name = display_name;
         this.seats = seats;
         this.image_url = image_url;
         this.pricing_details = pricing_details;
+        this.scheduled_pricing_details = scheduled_pricing_details;
     }
 
     @Override
@@ -44,6 +55,7 @@ public class RideType implements Validatable {
         sb.append("  seats: ").append(seats).append("\n");
         sb.append("  image_url: ").append(image_url).append("\n");
         sb.append("  pricing_details: ").append(pricing_details).append("\n");
+        sb.append("  scheduled_pricing_details: ").append(scheduled_pricing_details).append("\n");
         sb.append("}\n");
         return sb.toString();
     }
